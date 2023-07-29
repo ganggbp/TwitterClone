3b84c4be025bde3fb13dcdd6479a6989f43168c5
gangg <burit_bk@hotmail.com>
2023-07-29 19:11:43 +0700

    add new action icon

--- TwitterClone/Controllers/Core/HomeViewController.swift
+++ TwitterClone/Controllers/Core/HomeViewController.swift
@@ -8,6 +8,25 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private func configureNavigationBar() {
+        let size: CGFloat = 36
+        let logoImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        logoImageView.contentMode = .scaleAspectFit
+        logoImageView.image = UIImage(named: "twitterLogo")
+        
+        let middleView = UIView(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        middleView.addSubview(logoImageView)
+        navigationItem.titleView = middleView
+        
+        let profileImage = UIImage(systemName: "person")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: profileImage, style: .plain, target: self, action: #selector(didTapProfile))
+    }
+    
+    @objc private func didTapProfile() {
+        let vc = ProfileViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
     private let timelineTableView: UITableView = {
         let tableView = UITableView()
@@ -21,6 +40,7 @@ class HomeViewController: UIViewController {
         
         timelineTableView.delegate = self
         timelineTableView.dataSource = self
+        configureNavigationBar()
     }
     
     override func viewDidLayoutSubviews() {
---
+++ TwitterClone/Controllers/General/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  TwitterClone
+//
+//  Created by Burit Boonkorn on 29/7/2566 BE.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Profile"
+    }
+}
